2d14a290b0a64e8666a1c56006bc8fac5202c67a
Yosuke Tanigawa <ytanigaw@stanford.edu>
2020-04-08 18:25:18 -0700

    update run scripts for the Bejerano lab machines

--- run-bejerano.sh
+++ run-bejerano.sh
@@ -7,14 +7,14 @@ if [ $# -gt 1 ] ; then version=$1 ; else version=${DEFAULT_VERSION} ; fi
 port="8890"
 
 dimg="yosuketanigawa/jupyter_yt:${version}"
-bind_dst="/home/ytanigaw"
-tmp_dir=/tmp/u/$USER/jupyter
+bind_dst="/cluster/u/$USER"
+tmp_dir=/tmp/docker-jupyter
 if [ ! -d $tmp_dir ] ; then mkdir -p $tmp_dir ; fi
+chmod 777 ${tmp_dir}
 
-docker run -it -w ${bind_dst} --rm -p ${port}:8888 \
+docker run -it \
+--user=root -e NB_USER=jovyan -e NB_UID=17737 -e NB_GID=559 \
+-w ${bind_dst} --rm -p ${port}:8888 \
 --mount type=bind,src=/cluster/u/$USER,dst=${bind_dst} \
---mount type=bind,src=/cluster,dst=/cluster \
 --mount type=bind,src=${tmp_dir},dst=/jupyter-runtime \
-${dimg} \
-/opt/jupyter-start.sh
-
+${dimg} 
