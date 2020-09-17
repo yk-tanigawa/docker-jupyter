c7c870b5302a78dee511f1e1c8872d5f3aff095b
Yosuke Tanigawa <yosuke.tanigawa@stanford.edu>
2020-09-16 17:21:30 -0700

    add version 20200915 (regular update)

--- docker.update.push.sh
+++ docker.update.push.sh
@@ -3,7 +3,7 @@ set -beEuo pipefail
 
 version=$1
 
-docker build -t jupyter_yt:${version} .
+docker build --no-cache -t jupyter_yt:${version} .
 image_id=$(docker images | awk '(NR==2){print $3}')
 docker tag ${image_id} yosuketanigawa/jupyter_yt:${version}
 docker push yosuketanigawa/jupyter_yt:${version}
