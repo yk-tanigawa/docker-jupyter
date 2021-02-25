e4f92f3781ba610ace4c37d7dfbbafc5b4387867
Yosuke Tanigawa <ytanigaw@stanford.edu>
2021-02-25 10:18:04 -0800

    update default version

--- run-misc.sh
+++ run-misc.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -beEuo pipefail
 
-[[ ${DEFAULT_VERSION:-} -eq 1 ]] && return || readonly DEFAULT_VERSION="20210130"
+[[ ${DEFAULT_VERSION:-} -eq 1 ]] && return || readonly DEFAULT_VERSION="20210224"
 
